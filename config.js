66afd90b1c07d7fae88886615aabd66498aa9be9
patrykpawelchojnacki-commits <patryk.pawel.chojnacki@gmail.com>
2026-06-30 23:02:01 +0200

    config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // KONFIGURACJA - wpisz tu dane swojego projektu Supabase
 // Supabase Dashboard -> Project Settings -> API
 // =========================================================
-const SUPABASE_URL = "https://mszoffwbrksiietxevxi.co";
+const SUPABASE_URL = "https://mszoffwbrksiietxevxi.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zem9mZndicmtzaWlldHhldnhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4NDIwMTYsImV4cCI6MjA5ODQxODAxNn0.TuWdUtmCdga4dB1hbLCXEUw8K2D6iWb5T4Z6VqWDriw";
 
 // hasło do panelu admina (apka 3) - zmień na własne
